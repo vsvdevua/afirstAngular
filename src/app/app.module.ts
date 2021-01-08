21f71515bc55ca3ed769b28d73c9345cfba8c3f1
vsvdevua <vsvdevua@online.ua>
2021-01-08 14:19:38 +0200

    added lib bootstrap-css-only, angular material and component and service (static)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './views/categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule

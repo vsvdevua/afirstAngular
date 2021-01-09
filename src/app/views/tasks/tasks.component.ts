141d6e72ec8e5cd6533923a1e0cea4d998cc7f8b
vsvdevua <vsvdevua@online.ua>
2021-01-09 15:03:12 +0200

    added sort and pagination, and work without data

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,17 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {DataHandlerService} from '../../service/data-handler.service';
 import {Task} from '../../model/Task';
 import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit {
-  public displayedColumns: string[] = ['color', 'id', 'title', 'priority', 'category'];
- public dataSource: MatTableDataSource<Task>;
+export class TasksComponent implements OnInit, AfterViewInit {
+  private displayedColumns: string[] = ['color', 'id', 'title', 'priority', 'category'];
+  private dataSource: MatTableDataSource<Task>;
   tasks: Task[];
+  @ViewChild(MatPaginator, {static: false}) private paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) private sort: MatSort;
 
   constructor(private dataHandler: DataHandlerService) {
   }
@@ -28,15 +32,47 @@ export class TasksComponent implements OnInit {
   }
 
   // tslint:disable-next-line:typedef
-  public getPriorityColor(task: Task) {
+  private getPriorityColor(task: Task) {
     if (task.priority && task.priority.color) {
       return task.priority.color;
+    }
+    if (task.completed) {
+      return '#F8F9FA';
     }
     return '#fff';
   }
 
   // tslint:disable-next-line:typedef
- public refreshTable() {
+  private refreshTable() {
     this.dataSource.data = this.tasks;
+    this.addTableObjects();
+    this.dataSource.sortingDataAccessor = (task, colName) => {
+      switch (colName) {
+        case 'priority': {
+          return task.priority ? task.priority.id : null;
+        }
+        case 'category' : {
+          return task.category ? task.category.title : null;
+        }
+        case 'data' : {
+          return task.date ? task.date : null;
+        }
+        case 'title' : {
+          return task.title;
+        }
+
+      }
+    };
+  }
+
+  ngAfterViewInit(): void {
+    this.addTableObjects();
+  }
+
+
+  // tslint:disable-next-line:typedef
+  private addTableObjects() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 }

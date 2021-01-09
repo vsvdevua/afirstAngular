290ed17defd1166d497055bcedc4f5ba363386f1
vsvdevua <vsvdevua@online.ua>
2021-01-09 14:04:22 +0200

    set view to mathtable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './views/categories/categories.component';
 import { TasksComponent } from './views/tasks/tasks.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,11 @@ import { TasksComponent } from './views/tasks/tasks.component';
     TasksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {DataHandlerService} from '../../service/data-handler.service';
 import {Task} from '../../model/Task';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-tasks',
@@ -8,6 +9,8 @@ import {Task} from '../../model/Task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
+  public displayedColumns: string[] = ['color', 'id', 'title', 'priority', 'category'];
+ public dataSource: MatTableDataSource<Task>;
   tasks: Task[];
 
   constructor(private dataHandler: DataHandlerService) {
@@ -15,10 +18,25 @@ export class TasksComponent implements OnInit {
 
   ngOnInit(): void {
     this.dataHandler.tasksSubject.subscribe(tasks => this.tasks = tasks);
+    this.dataSource = new MatTableDataSource();
+    this.refreshTable();
   }
 
   // tslint:disable-next-line:typedef
   toggleTaskCompleted(task: Task) {
     task.completed = !task.completed;
+  }
+
+  // tslint:disable-next-line:typedef
+  public getPriorityColor(task: Task) {
+    if (task.priority && task.priority.color) {
+      return task.priority.color;
+    }
+    return '#fff';
+  }
+
+  // tslint:disable-next-line:typedef
+ public refreshTable() {
+    this.dataSource.data = this.tasks;
   }
 }
